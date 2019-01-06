a7304ce8fb5b2eb60f872f1f5b965048963c2fe7
Agil Isayev <isayevagil@gmail.com>
2019-01-06 23:04:18 +0400

    first commit

---
+++ src/car/road/traffic_2/Node.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car.road.traffic_2;
+
+/**
+ *
+ * @author AgilI  
+ */
+public class Node {
+
+    double x;
+    double y;
+    int lane;
+    int road;
+    double dx;
+    double dy;
+    String lbl;
+    int carW;
+    int carL;
+    double carWaiting;
+}
