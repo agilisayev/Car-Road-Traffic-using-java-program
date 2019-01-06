b151174b932847be12743576e63a03ecb8962b79
Agil Isayev <isayevagil@gmail.com>
2019-01-06 22:49:27 +0400

    first commit

---
+++ src/car/road/traffic/Vertical.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car.road.traffic;
+
+/**
+ *
+ * @author AgilI
+ * 
+ */
+public class Vertical {
+
+    public int x;
+    public int y;
+
+//    public Vertical() {
+//        System.out.println("default");
+//    }
+
+    public Vertical(int vertical, int horizontal) {
+        this.x = vertical;
+        this.y = horizontal;
+    }
+
+}
